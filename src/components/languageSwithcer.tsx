3bdb69ef6346102e45f816125c3029cc630a0804
boomin7x <kodiahbetro@gmail.com>
2025-09-10 02:29:30 +0100

    Update dependencies and enhance language support
    - Added new dependencies: country-list-with-dial-code-and-flag and recoil for improved language switching functionality.
    - Updated React and React DOM versions to 18.2.0 for better performance and compatibility.
    - Refactored i18n setup to directly import translation files for English and French.
    - Enhanced LanguageSwitcher component to display country flags alongside language options.
    - Integrated Recoil for state management in layout components, improving sidebar visibility control.
    - Updated translation files to include new keys for home and sign-in pages.
    - Improved responsiveness and styling in layout components for a better user experience.

--- src/components/languageSwithcer.tsx
+++ src/components/languageSwithcer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { useParams, useNavigate } from "react-router-dom";
